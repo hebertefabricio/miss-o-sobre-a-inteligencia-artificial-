d3ca52d33028b1a8b7283a4c9f28d47e2c9c17be
hebertefabricio <148267252+hebertefabricio@users.noreply.github.com>
2025-11-26 14:13:35 +0000

    inicio

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa-principal"); 
+const caixaperguntas = document.querySelector(".csixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas"); 
+const caixasResultados = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".testo-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativas 1",
+            "Alternaivas 2"
+        ]
+    }
+];
